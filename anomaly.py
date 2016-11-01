f5c07daf62a3293d0cfffa73950b1b6d84eda024
Raunaq Saxena <raunaqsaxena23@gmail.com>
2016-11-01 14:53:25 -0400

    with algorithm commit1

--- anomaly.py
+++ anomaly.py
@@ -2,6 +2,10 @@ from os import listdir
 from os.path import isfile, join
 import numpy as np
 import networkx as nx
+import random as rm
+from numpy import matrix
+from scipy.linalg import solve
+import sys
 
 class Anomaly:
 	file_path = None
@@ -18,7 +22,7 @@ class Anomaly:
 			self.file_list = [f for f in listdir(self.file_path) if isfile(join(self.file_path, f))]
 			self.file_list = sorted(self.file_list,cmp=compare)
 			self.file_list_size = len(self.file_list)
-			print self.file_list
+			#print self.file_list
 
 	def loadEdgeVertices(self,index):
 		if self.index == self.file_list_size:
@@ -29,11 +33,11 @@ class Anomaly:
 		self.vertices = range(int(edges[0].split(" ")[0]))
 			
 		self.edge_lists = map(lambda x:(int(x.split(" ")[0]),int(x.split(" ")[1])),edges[1:])
-		print self.vertices
-		print self.edge_lists
+		#print self.vertices
+		#print self.edge_lists
 
 	def makeGraph(self):
-		if self.index == self.file_list_size:
+		if self.index == self.file_list_size - 1:
 			return False
 		self.loadEdgeVertices(self.index)
 		G1 = nx.Graph()
@@ -46,6 +50,9 @@ class Anomaly:
 		self.index = self.index + 1
 		A1,D1,max1 = self.makeDiagonal(G1)
 		A2,D2,max2 = self.makeDiagonal(G2)
+		ep = min([max1,max2])
+		results.append( self.getSimilarityScore(D1,A1,D2,A2,max1,max2,self.vertices)  )
+		#print results[-1]
 		return True
 
 	def makeDiagonal(self,G):
@@ -62,13 +69,53 @@ class Anomaly:
 			count = count + 1
 		return zero,float(1/float(1 + max_degree))
 
+	def makeGroups(self, vertex):
+		threshold = int(np.sqrt(len(vertex)))
+		g = rm.randint(threshold / 2, threshold)
+		groups = [list() for x in xrange(g)]
+
+		for elem in vertex:
+			groups[rm.randint(0, g - 1)].append(elem)
+
+		return groups
+
+	def getSimilarityScore(self, diag1, adj1, diag2, adj2, epsilon1, epsilon2, vertex):
+		groups = self.makeGroups(vertex)
+		A1 = matrix(np.identity(len(diag1)) + ((epsilon1 * epsilon1) * diag1) - (epsilon1 * adj1))
+		A2 = matrix(np.identity(len(diag2)) + ((epsilon2 * epsilon2) * diag2) - (epsilon2 * adj2))
+		rootedDist = 0.0
+		N = len(vertex)
+		# Commented out matrix creation as maybe not needed to get Similarity Score
+		# S1= list()
+		# S2= list()
+		for group in groups:
+			group = set(group)
+			groupRow = [1 if index in group else 0 for index in xrange(N)]
+			groupCol = matrix(groupRow).T
+			_s1 = solve(A1, groupCol)
+			_s2 = solve(A2, groupCol)
+			rootedDist += sum([pow(np.sqrt(x) - np.sqrt(y), 2) for x, y in zip(_s1, _s2)])
+		# S1.append(_s1)
+		# S2.append(_s2)
+		# S1 = matrix(S1).T
+		# S2 = matrix(S2).T
+		rootedDist = np.sqrt(rootedDist)
+		return 1 / (1 + rootedDist)
+
 def compare(file1,file2):
 	return int(file1.split('_')[0]) - int(file2.split('_')[0])
 
 def main():
-	a = Anomaly("/home/abhishek/Downloads/anomaly/datasets/datasets/enron_by_day/")
+	global results
+	results = list()
+	#dirPath = "/home/abhishek/Downloads/anomaly/datasets/datasets/enron_by_day/"
+	dirPath = '/home/raunaq/College/GDM/project4/anomaly/datasets/datasets/enron_by_day/'
+	if len(sys.argv)>1:
+		dirPath = str(sys.argv[1])
+	a = Anomaly(dirPath)
 	a.load_filename()
-	a.makeGraph()
+	while a.makeGraph():
+		pass
 
 if __name__ == '__main__':
 	main()
